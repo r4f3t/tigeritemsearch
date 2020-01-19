efdda804a762f1bf524ae37c282f3d1b9041c577
r4f3t <parlakrafet01@gmail.com>
2020-01-19 14:06:52 +0300

    to branch test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 552);
+            this.ClientSize = new System.Drawing.Size(782, 552);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
